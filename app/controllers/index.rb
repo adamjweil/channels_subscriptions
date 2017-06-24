20f6f157ccda33fe6bec0031de1dc6036a254b45
Evans Wang <evansjwang@gmail.com>
2017-06-24 17:59:28 -0400

    Need to include update to USERS to add additional channels. Need to display without logging in all channels. Need unsubscribe button

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,16 +11,28 @@ end
 post '/Users/login' do
   if User.authenticate(params[:users])
     session[:user_id] = User.find_by(email: params[:users][:email]).id
-    redirect '/'
+    redirect '/Users/:id'
   else
     "Login Failed"
   end
+end
+
+get '/Users/:id' do
+  @subscriptions = User.find_by(id: session[:user_id]).subscription
+
+  erb :'Users/show'
 end
 
 
 get '/User/new' do
 
   erb :'Users/new'
+end
+
+get '/subscription/:id' do
+  @subscriptions = User.find_by(id: session[:user_id])
+
+  erb :'subscription/show'
 end
 
 post '/User/new' do
@@ -32,6 +44,19 @@ post '/User/new' do
   else
     @user = @user.errors.full_messages
     erb :'Users/new'
+  end
+end
+
+post '/User/update' do
+  p session
+  p params
+  @subscription = Subscription.find_by(user_id: session[:user_id])
+  @subscription.assign_attributes(params[:user])
+  if @user.save
+    redirect :'Users/show'
+  else
+    @errors = @user.erros.full_messages
+    erb :'Users/edit'
   end
 end
 
