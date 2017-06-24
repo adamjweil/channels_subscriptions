7497619311dcbc54266a73bbffe6dfaed03b26d8
Wan Tsui <wanytsui@gmail.com>
2017-06-24 12:30:31 -0400

    Add signup form with error messages logic and a success page. Update users controller. Start a new view for sessions for logging in.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,16 @@
-
-# 
 get '/users/new' do
+  @user = User.new
+  erb :'/users/new_user'
+end
 
+post '/users' do
+  @user = User.new(
+    params[:user]
+  )
+  if @user.valid?
+    @user.save
+    erb :'/users/success'
+  else
+    erb :'/users/new_user'
+  end
 end
