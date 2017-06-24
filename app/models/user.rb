c1901d5d9a88477d65102525fbd0ec6afa39eff4
Rakey Drammeh <rdrammeh@gmail.com>
2017-06-24 13:25:08 -0400

    Create 'authenticate' method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   validates :email, :hashed_password, presence: true
   validates :email, uniqueness: true
 
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+      if @user.password == password && @user.email == email
+        return @user
+      else
+        nil
+      end
+  end
+
   def password
     @password ||= BCrypt::Password.new(hashed_password)
   end
