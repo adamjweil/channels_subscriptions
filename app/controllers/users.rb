322080b401c75ba77021d62081910cc823a4d537
Eddie Harmon <eddieharmon@hotmail.com>
2017-06-25 11:11:35 -0400

    Add user view file with an HTML form. Add users get and post routes to user controller. Change user table column from password to hashed_password.

---
+++ app/controllers/users.rb
@@ -0,0 +1,8 @@
+get '/users/new' do
+  "Form to create new user"
+  erb :'/users'
+end
+
+post '/users' do
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   # Remember to create a migration!
+  validates :email, presence: true, uniqueness: true
+  validates :hashed_password, presence: true
 end
