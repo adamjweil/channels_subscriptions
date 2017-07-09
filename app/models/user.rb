032c266ba7896ee0af4fa69e61718c5c212f8197
Eddie Harmon <eddieharmon@hotmail.com>
2017-07-09 19:51:49 -0400

    add email and password validations and bcrypt methods to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
-  # Remember to create a migration!
+  validates :email, presence: true, unique: true
+  validates :password, presence: true
+end
+
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
+
 end
