6887515a46a69b435fe6b59b4f5195ef44e71bf8
Eddie Harmon <eddieharmon@hotmail.com>
2017-07-10 19:18:33 -0400

    add user session upon registration. Add logout link to layout if logged in.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,7 +10,8 @@ end
 post '/users' do
   @user = User.new(params[:user])
   if @user.save
-    redirect '/users'
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
   else
     erb :'/users/new'
   end
