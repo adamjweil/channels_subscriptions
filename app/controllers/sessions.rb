e5d5cacb54a349d2378acb4a7db9c617e1c6cb0d
Iftekhar Alam <email": null>
2017-07-11 10:42:16 -0400

    Initial Commit

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,36 @@
+get '/users/new' do
+  erb :'/users/new', layout: false
+end
+
+get '/users/:id' do
+  if logged_in?
+    @user = User.find_by(id: session[:user_id])
+    erb :'users/show'
+  else
+    erb :'/index'
+  end
+end
+
+get '/users' do
+  erb :'/users/sessions', layout: false
+end
+
+
+post '/users' do
+  User.create(params[:user])
+  redirect '/'
+end
+
+post '/sessions' do
+  if User.authenticate(params[:user])
+    session[:user_id] = User.find_by(email: params[:user][:email]).id
+    redirect "/users/#{session[:user_id]}"
+  else
+    "Login Failed"
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
   # Remember to create a migration!
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  // ***************************************************
+  def self.authenticate(login_credentials)
+    user = User.find_by(email: login_credentials[:email])
+    if user
+      user.password == login_credentials[:password]
+    else
+      false
+    end
+  end
+  // **************************************************
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+  //***************************************************
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+  // --------------------------------------------------
 end
