56af7a51595725cb1523ab6aca5891ee4bdeaf19
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-11 11:35:12 -0400

    sunnys updates

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -5,19 +5,5 @@ end
 
 get '/channels/:id' do
   @channel = Channel.find_by(id: params[:id])
-  @all_channels = Channel.all
   erb :'/channels/show'
-end
-
-
-put '/channels/:id' do
-  @channel = Channel.find_by(id: params[:id])
-  redirect 'users/:id'
-end
-
-
-delete '/channels/:id' do
-  @channel = Channel.find_by(id: params[:id])
-  @channel.destroy
-  redirect '/users/:id'
 end
